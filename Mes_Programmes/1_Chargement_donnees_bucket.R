3402ae7ba35d9dd1c674e1818354b95694ca54a5
m2909b <mickael.brefort@insee.fr>
2022-09-23 15:06:55 +0000

    Ajout de programmes

---
+++ Mes_Programmes/1_Chargement_donnees_bucket.R
@@ -0,0 +1,56 @@
+
+#---------------------------------------------------------------#
+# Chargement d'une table de données depuis mon bucket personnel #
+#---------------------------------------------------------------#
+
+# Cf https://www.book.utilitr.org/sspcloud.html  (droits daccès au s3)
+source("credentials.R")
+
+
+# install.packages("aws.s3", repos = "https://www.book.utilitr.org/sspcloud.html")
+library("aws.s3")
+
+bucketlist(region="")
+
+#ex : données au format RDS
+RP16_Ind_LR <- 
+  aws.s3::s3read_using(
+    FUN = readRDS,
+    # Mettre les options de FUN ici
+    object = "/Donnees_RP/RP16_Ind_LR.rds",
+    bucket = "m2909b",
+    opts = list("region" = "")
+  )
+
+
+library(tidyverse)
+RP16_Ind_LR %>% colnames()
+
+
+# Sauvegarder un fichier dans son bucket personnel
+library(doremifasolData)
+RP_Boulogne <- RP16_Ind_LR %>%
+  ungroup() %>% 
+  filter(CODGEO == "62160")
+
+aws.s3::s3write_using(
+  RP_Boulogne,
+  FUN = saveRDS,
+  # Les options de fread sont ici
+  object = "Donnees_RP/RP_Boulogne.RDS",
+  bucket = "m2909b",
+  opts = list("region" = "")
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
