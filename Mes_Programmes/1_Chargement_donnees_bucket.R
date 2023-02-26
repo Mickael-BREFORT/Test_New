07c9679f3af77f61d9263e6bfc02642dacdda047
m2909b <mickael.brefort@insee.fr>
2023-02-26 13:16:08 +0000

    # ajout d'un nouveau programme

--- Mes_Programmes/1_Chargement_donnees_bucket.R
+++ Mes_Programmes/1_Chargement_donnees_bucket.R
@@ -4,6 +4,29 @@
 #---------------------------------------------------------------#
 
 # test d'une modification !!
+# modif2 !!
+
+df <- 
+  aws.s3::s3read_using(
+    FUN = readr::read_csv2,
+    object = "diffusion/bonnes-pratiques-r/rp_2016_individu_sample.csv",
+    bucket = "projet-formation",
+    opts = list("region" = "")
+  )
+
+readr::write_csv2(df, "individu_reg.csv")
+
+library(tidyverse)
+
+# j'importe les données avec read_csv2 parce que c'est un csv avec des ; et que read_csv attend comme separateur des , 
+df <- readr::read_csv2(
+  "individu_reg.csv",
+  col_select = c("region", "aemm", "aged", "anai","catl","cs1", "cs2", "cs3", "couple", "na38", "naf08", "pnai12", "sexe", "surf", "tp", "trans", "ur"))
+
+
+
+
+
 
 # Cf https://www.book.utilitr.org/sspcloud.html  (droits daccÃ¨s au s3)
 source("credentials.R")
